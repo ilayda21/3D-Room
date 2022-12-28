73d7451b76e10a005f8f5198429c8713dbe7fc1d
Ilayda <ilayda@sparelabs.com>
2022-12-28 12:03:00 -0800

    Add camera movements

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -17,6 +17,10 @@ export default class Controls {
     this.position = new THREE.Vector3(0, 0, 0);
     this.lookAtPosition = new THREE.Vector3(0, 0, 0);
 
+    this.directionalVector = new THREE.Vector3(0, 0, 0);
+    this.staticVector = new THREE.Vector3(0, 1, 0);
+    this.crossVector = new THREE.Vector3(0, 0, 0);
+
     this.setPath();
     this.onWheel();
   }
@@ -25,11 +29,10 @@ export default class Controls {
     //Create a closed wavey loop
     this.curve = new THREE.CatmullRomCurve3(
       [
-        new THREE.Vector3(-10, 0, 10),
-        new THREE.Vector3(-5, 5, 5),
-        new THREE.Vector3(0, 0, 0),
-        new THREE.Vector3(5, -5, 5),
-        new THREE.Vector3(10, 0, 10),
+        new THREE.Vector3(-5, 0, 0),
+        new THREE.Vector3(0, 0, -5),
+        new THREE.Vector3(5, 0, 0),
+        new THREE.Vector3(0, 0, 5),
       ],
       true
     );
@@ -53,6 +56,10 @@ export default class Controls {
       } else {
         this.lerp.target -= 0.01;
         this.back = true;
+
+        if (this.lerp.target < 0) {
+          this.lerp.target = 0;
+        }
       }
     });
   }
@@ -64,21 +71,36 @@ export default class Controls {
       this.lerp.ease
     );
 
-    if (this.back) {
-      this.lerp.target -= 0.001;
-    } else {
-      this.lerp.target += 0.001;
-    }
-
-    this.lerp.target = GSAP.utils.clamp(0, 1, this.lerp.target);
-    this.lerp.current = GSAP.utils.clamp(0, 1, this.lerp.current);
-    this.curve.getPointAt(this.lerp.current, this.position);
-    this.curve.getPointAt(
-      GSAP.utils.clamp(0, 1, this.lerp.current + 0.00001),
-      this.lookAtPosition
-    );
-
+    // set Camera position
+    this.curve.getPointAt(this.lerp.current % 1, this.position);
     this.camera.orthographicCamera.position.copy(this.position);
-    this.camera.orthographicCamera.lookAt(this.lookAtPosition);
+
+    // make camera look inwards
+    this.directionalVector.subVectors(
+      this.curve.getPointAt((this.lerp.current + 0.000001) % 1),
+      this.position
+    );
+    this.directionalVector.normalize();
+
+    this.crossVector.crossVectors(this.directionalVector, this.staticVector);
+    this.crossVector.multiplyScalar(100000);
+    this.camera.orthographicCamera.lookAt(this.crossVector);
+
+    // if (this.back) {
+    //   this.lerp.target -= 0.001;
+    // } else {
+    //   this.lerp.target += 0.001;
+    // }
+    // this.lerp.target = GSAP.utils.clamp(0, 1 - 0.00001, this.lerp.target);
+    // this.lerp.current = GSAP.utils.clamp(0, 1 - 0.00001, this.lerp.current);
+    // this.curve.getPointAt(this.lerp.current, this.position);
+    // this.curve.getPointAt(
+    //   GSAP.utils.clamp(0, 1, this.lerp.current + 0.00001),
+    //   this.lookAtPosition
+    // );
+    // // Move camera with given position
+    // this.camera.orthographicCamera.position.copy(this.position);
+    // // Make camera turn at the given direction
+    // this.camera.orthographicCamera.lookAt(this.lookAtPosition);
   }
 }
